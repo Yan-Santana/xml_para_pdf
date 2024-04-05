21db02b66d6ed8faa491bca8d9c86ecc2e435764
Yan_Santana <yansantana63@gmail.com>
2024-04-05 12:20:48 -0400

    Feat: adjusting for my XML file

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from tkinter import filedialog, messagebox
 from fpdf import FPDF
 import xml.etree.ElementTree as ET
 
-
 class XMLtoPDFConverter:
     def __init__(self, root):
         self.root = root
@@ -49,22 +48,47 @@ class XMLtoPDFConverter:
         pdf_path = self.output_folder + "/nota_fiscal.pdf"
         self.create_pdf(informacoes_nota, pdf_path)
         messagebox.showinfo("Conversão Concluída", "A nota fiscal foi convertida para PDF com sucesso!")
-
+        
     def extract_information_from_xml(self, xml_path):
         informacoes = {}
+        ns = {'nfe': 'http://www.portalfiscal.inf.br/nfe'}  
         tree = ET.parse(xml_path)
         root = tree.getroot()
-        
-        for child in root:
-            if child.tag == 'produtos':
-                informacoes[child.tag] = []
-                for produto in child:
-                    produto_info = {}
-                    for info in produto:
-                        produto_info[info.tag] = info.text
-                    informacoes[child.tag].append(produto_info)
-            else:
-                informacoes[child.tag] = child.text
+
+        # Extração das informações básicas (exemplo: emitente, destinatário)
+        for child in root.findall('.//nfe:infNFe', ns):
+            for info in child:
+                if info.tag.endswith('emit') or info.tag.endswith('dest'):
+                    # Trata informações - emitente e destinatário
+                    section_info = {}
+                    for detail in info:
+                        detail_tag = detail.tag.replace('{http://www.portalfiscal.inf.br/nfe}', '')
+                        if detail.tag.endswith('enderEmit') or detail.tag.endswith('enderDest'):
+                            # Endereço do emitente ou destinatário
+                            address_info = {}
+                            for address_detail in detail:
+                                address_tag = address_detail.tag.replace('{http://www.portalfiscal.inf.br/nfe}', '')
+                                address_info[address_tag] = address_detail.text
+                            section_info[detail_tag] = address_info
+                        else:
+                            section_info[detail_tag] = detail.text
+                    informacoes[info.tag.replace('{http://www.portalfiscal.inf.br/nfe}', '')] = section_info
+                elif info.tag.endswith('det'):
+                    # Trata detalhes dos produtos
+                    if 'prod' not in informacoes:
+                        informacoes['prod'] = []
+                    prod_info = {}
+                    imposto_info = {}
+                    for prod_or_imposto in info:
+                        if prod_or_imposto.tag.endswith('prod'):
+                            for prod_detail in prod_or_imposto:
+                                prod_tag = prod_detail.tag.replace('{http://www.portalfiscal.inf.br/nfe}', '')
+                                prod_info[prod_tag] = prod_detail.text
+                        elif prod_or_imposto.tag.endswith('imposto'):
+                            for imposto_detail in prod_or_imposto:
+                                imposto_tag = imposto_detail.tag.replace('{http://www.portalfiscal.inf.br/nfe}', '')
+                                imposto_info[imposto_tag] = "Informação de imposto"  
+                    informacoes['prod'].append({'prod_info': prod_info, 'imposto_info': imposto_info})
 
         return informacoes
 
@@ -73,14 +97,31 @@ class XMLtoPDFConverter:
         pdf.add_page()
         pdf.set_font("Arial", size=12)
 
-        for chave, valor in informacoes.items():
-            if isinstance(valor, list):  # Se for uma lista (produtos)
-                pdf.cell(200, 10, txt=f"{chave}:", ln=True, align='L')
-                for produto in valor:
-                    for produto_chave, produto_valor in produto.items():
-                        pdf.cell(200, 10, txt=f"{produto_chave}: {produto_valor}", ln=True, align='L')
-            else:
-                pdf.cell(200, 10, txt=f"{chave}: {valor}", ln=True, align='L')
+        for section, details in informacoes.items():
+            if section in ['emit', 'dest']:
+                pdf.cell(0, 10, f"{section.upper()}:", 0, 1)
+                for key, value in details.items():
+                    if isinstance(value, dict):  
+                        for sub_key, sub_value in value.items():
+                            pdf.cell(0, 10, f"{sub_key}: {sub_value}", 0, 1)
+                    else:
+                        pdf.cell(0, 10, f"{key}: {value}", 0, 1)
+                pdf.cell(0, 10, "", 0, 1) 
+
+            elif section == 'prod':
+                pdf.cell(0, 10, "PRODUTOS:", 0, 1)
+                for product in details:
+                    pdf.set_font("Arial", 'B', 12)  
+                    pdf.cell(0, 10, f"Produto {details.index(product) + 1}", 0, 1)
+                    pdf.set_font("Arial", size=12)  
+                    for prod_key, prod_value in product['prod_info'].items():
+                        pdf.cell(0, 10, f"{prod_key}: {prod_value}", 0, 1)
+                    
+                    pdf.cell(0, 10, "Imposto:", 0, 1)
+                    for imposto_key, imposto_value in product['imposto_info'].items():
+                        pdf.cell(0, 10, f"{imposto_key}: {imposto_value}", 0, 1)
+                    
+                    pdf.cell(0, 10, "", 0, 1)  
 
         pdf.output(pdf_path)
 
